abeac7a3612df0405cb90bec43e8caaf369f1fe9
Lordmatics <niallmaple@yahoo.co.uk>
2018-01-11 23:58:55 +0000

    PacMan Setup
    Player and Dots

---
+++ TestProject/Assets/Scripts/PacDots.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class PacDots : MonoBehaviour 
+{
+
+	private Rigidbody2D 		m_rb;
+	private CircleCollider2D 	m_col;
+
+	void Awake()
+	{
+		m_rb = GetComponent<Rigidbody2D> ();
+		m_col = GetComponent<CircleCollider2D> ();
+	}
+
+	void Start () 
+	{
+		m_col.isTrigger = true;
+		m_rb.isKinematic = true;
+		m_rb.freezeRotation = true;
+	}
+
+	void Update () 
+	{
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+	}
+
+	public void OnEaten()
+	{
+		Destroy (gameObject);
+	}
+}
---
+++ TestProject/Assets/Scripts/PacmanController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class PacmanController : MonoBehaviour 
+{
+
+	private Rigidbody2D 		m_rb;
+	private CircleCollider2D 	m_col;
+
+	[SerializeField]
+	private float m_moveSpeed;
+
+	void Awake()
+	{
+		m_rb = GetComponent<Rigidbody2D> ();
+		m_col = GetComponent<CircleCollider2D> ();
+	}
+
+	void Start () 
+	{
+		m_col.isTrigger = true;
+		m_rb.isKinematic = true;
+		m_rb.freezeRotation = true;
+		m_moveSpeed = 5.0f;
+	}
+	
+	void Update () 
+	{
+		float h = Input.GetAxis ("Horizontal");
+
+		Vector3 moveVec = h * m_moveSpeed * Time.deltaTime * Vector3.right;
+		transform.Translate (moveVec);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		PacDots script = other.gameObject.GetComponent<PacDots> ();
+		if (script != null) 
+		{
+			script.OnEaten ();
+		}
+	}
+}
